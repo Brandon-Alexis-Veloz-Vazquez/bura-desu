b3cb75bfeb28305f7e8a41ace6e1ea4ac853d07f
Brandon Alexis Veloz Vazquez <desarrollo13@copsis.com>
2022-02-10 12:02:03 -0600

    Se quito objeto de particleJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,84 +11,9 @@ declare var particlesJS: any;
 })
 export class AppComponent implements OnInit {
   id = "tsparticles";
-  particlesOptions = {
-    background: {
-        color: {
-            value: "#000000"
-        }
-    },
-    fpsLimit: 120,
-    interactivity: {
-        events: {
-            onClick: {
-                enable: true,
-                mode: "push"
-            },
-            onHover: {
-                enable: true,
-                mode: "repulse"
-            },
-            resize: true
-        },
-        modes: {
-            bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40
-            },
-            push: {
-                quantity: 4
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4
-            }
-        }
-    },
-    particles: {
-        color: {
-            value: "#ffffff"
-        },
-        links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1
-        },
-        move: {
-            direction: "none",
-            enable: true,
-            outModes: "bounce",
-            random: false,
-            speed: 6,
-            straight: false
-        },
-        number: {
-            density: {
-                enable: true,
-                area: 800
-            },
-            value: 80
-        },
-        opacity: {
-            value: 0.5
-        },
-        shape: {
-            type: "circle"
-        },
-        size: {
-            value: { min: 1, max: 5 }
-        }
-    },
-    detectRetina: true
-};
-
 
   ngOnInit(): void {
-    /* particlesJS.load('particles-js', '../assets/json/particlesjs-config.json', null);
-    window.dispatchEvent(new Event('resize')); */
+    
   }
 
   particlesLoaded(container: Container): void {
